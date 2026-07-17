fe9aac14289f977f7a7e9091423be7d731577724
Thalia Crevilatti <thaliacrevilatti44@gmail.com>
2026-07-16 21:05:31 -0300

    Calculadora em Dart com contenções🚦

---
+++ bin/calculadora.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+  String? entrada = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"];
+
+  void soma(){
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao(){
+    print(numeroUm - numeroDois);
+  }
+
+  void multiplicacao(){
+    print(numeroUm * numeroDois);
+  }
+
+  void divisao(){
+    print(numeroUm / numeroDois);
+  }
+
+  void calcular() { 
+    switch (operacao){
+      case "+":
+        soma();
+        break;
+
+      case "-":
+        subtracao();
+        break;
+
+      case "*":
+        multiplicacao();
+        break;
+
+      case "/":
+        divisao();
+        break;
+        
+      default:
+        print("Operação inválida");
+    }
+  }  
+
+  void getOperacao() {
+  print("Digite a operação ${operacoes.toString()}:");
+  entrada = stdin.readLineSync()!;
+    if (entrada != null){
+      if(operacoes.contains(entrada)){
+        operacao = entrada!;
+      } else {
+        print("Operação invalida. Digite novamente.");
+        getOperacao();
+      }
+    }
+  }
+
+  print("Digite o primeiro número:");
+  entrada = stdin.readLineSync()!;
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada!);
+    }
+  }
+
+  getOperacao();
+
+  print("Digite o segundo número:");
+  entrada = stdin.readLineSync()!;
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada!);
+    }
+  }
+
+  print("O resultado da operação é:");
+
+  calcular();
+}
